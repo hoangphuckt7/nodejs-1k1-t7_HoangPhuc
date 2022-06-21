7d148866bd2e0e634e07396b8e53ed515d9a01d8
Hoang Phuc <hoangphuckt7@gmail.com>
2022-06-21 19:47:48 +0700

    commit

--- validates/tables.js
+++ validates/tables.js
@@ -7,7 +7,7 @@ const options = {
     status : { value: 'novalue'}
 }
 module.exports = {
-    validator:(body) => {
+    validator:(body, check) => {
             // Name
             body('name',util.format(notifyConfig.validate_form.NOTIFY_VALIDATE_NAME, options.name.min, options.name.max))
                 .isLength({min:options.name.min, max: options.name.max}),
@@ -17,7 +17,7 @@ module.exports = {
                 .isInt({gt:options.ordering.min, sl:options.ordering.max}),//gt: greater, sl: smaller
 
             // Status
-            body('status', notifyConfig.validate_form.NOTIFY_VALIDATE_STATUS).custom((value) =>{
+            check('status', notifyConfig.validate_form.NOTIFY_VALIDATE_STATUS).custom((value) =>{
                 return value !== options.status.value
 	    })
     }
